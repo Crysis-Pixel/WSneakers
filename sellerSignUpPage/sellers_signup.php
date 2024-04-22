63b7b1679a757f13bc611f02ba709f415032a2c7
Tahsinul Haque Wrudra <rudra483haque@gmail.com>
2024-04-22 18:38:04 +0600

    Updated the sellers signup input
    Sanitized the inputs so that it doesn't take any script as inputs. Also if input is kept empty, the database wont store data. And finally to check whether sign up doesn't take duplicate username. It runs a query to check whether the new username input is the same.

--- sellerSignUpPage/sellers_signup.php
+++ sellerSignUpPage/sellers_signup.php
@@ -42,20 +42,26 @@ if (isset($_POST["submit"])) {
     if (empty($_POST["phone"]) || empty($_POST["username"]) || empty($_POST["password"])) {
         echo "Please fillup the sign up form";
     } else {
-        $phone = $_POST["phone"];
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO seller (Phone, Username, Pwd) VALUES('$phone', '$username', '$hashedPassword')";
-
         $con = db::getInstance()->getConnection();
+        $phone = filter_input(INPUT_POST, "phone", FILTER_SANITIZE_SPECIAL_CHARS);
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
 
-        mysqli_query($con, $sql);
-        try {
-            echo "User is now registered";
-        } catch (mysqli_sql_exception) {
-            echo "Could not register user";
+        $checkDuplicateUsername = "SELECT Username FROM seller WHERE Username = '$username'";
+        $result = mysqli_query($con, $checkDuplicateUsername);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "The Username already exists";
+        } else {
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO seller (Phone, Username, Pwd) VALUES('$phone', '$username', '$hashedPassword')";
+
+            mysqli_query($con, $sql);
+            try {
+                echo "User is now registered";
+            } catch (mysqli_sql_exception) {
+                echo "Could not register user";
+            }
         }
     }
 }
